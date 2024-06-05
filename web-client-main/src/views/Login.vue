190b6315fb177ef85ba2c2f2abdcd76cb83ec697
Nero <paul.bogza@gmail.com>
2024-06-05 11:07:32 +0200

    reverted back all changes

--- web-client-main/src/views/Login.vue
+++ web-client-main/src/views/Login.vue
@@ -27,50 +27,6 @@
 </template>
 
 <script>
-import Keycloak from 'keycloak-js'
-import vue from 'vue'
-
-let initOptions = {
-  url: 'https://0.0.0.0:8445/auth', realm: 'keycloak-demo', clientId: 'vue-test-app', onLoad:'login-required'
-}
-
-let keycloak = Keycloak(initOptions);
-
-keycloak.init({ onLoad: initOptions.onLoad }).success((auth) =>{
-    
-    if(!auth) {
-      window.location.reload();
-    } else {
-      Vue.$log.info("Authenticated");
-    }
- 
-    new Vue({
-      render: h => h(App),
-    }).$mount('#app')
-  
-
-    localStorage.setItem("vue-token", keycloak.token);
-    localStorage.setItem("vue-refresh-token", keycloak.refreshToken);
-
-    setInterval(() =>{
-      keycloak.updateToken(70).success((refreshed)=>{
-        if (refreshed) {
-          Vue.$log.debug('Token refreshed'+ refreshed);
-        } else {
-          Vue.$log.warn('Token not refreshed, valid for '
-          + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
-        }
-      }).error(()=>{
-          Vue.$log.error('Failed to refresh token');
-      });
-
-
-    }, 60000)
-
-}).error(() =>{
-  Vue.$log.error("Authenticated Failed");
-});
-/*
 export default {
   setup(){},
   data(){
@@ -112,7 +68,7 @@ export default {
       //window.location="/";
     }
   }
-};*/
+}
 </script>
 
 <style scoped>
