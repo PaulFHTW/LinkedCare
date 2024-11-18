5d186c4150e5de974998f9848428da6d6eeee678
PaulFHTW <paul.bogza@gmail.com>
2024-11-18 09:01:39 +0100

    changed cringe vue packages

--- web-client-main/src/main.js
+++ web-client-main/src/main.js
@@ -1,5 +1,4 @@
-import * as Vue from 'vue'
-// import { createApp } from 'vue'
+import * as Vue from 'vue' 
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
